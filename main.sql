5a8709af89bc2f0b34fcc006195ff9a8a182e922
nhporti2 <nhporti2@gmail.com>
2021-05-03 01:26:29 -0500

    Update main.sql

--- main.sql
+++ main.sql
@@ -3,7 +3,7 @@
 CREATE TABLE models(
 	model_id SERIAL PRIMARY KEY,
 	vehicle_name VARCHAR(150),
-  car_serial_id INTEGER NOT NULL,
+  	car_serial_id INTEGER NOT NULL,
 	brand_id INTEGER NOT NULL,
 	FOREIGN KEY(car_serial_d) REFERENCES model(car_serial_id),
 	FOREIGN KEY(brand_id) REFERENCES brand(brand_id);
@@ -13,37 +13,37 @@ CREATE TABLE models(
 CREATE TABLE customer(
 	customer_id SERIAL PRIMARY KEY,
 	first_name VARCHAR(100),
-  last_name VARCHAR(100),
-  address VARCHAR(150),
-  biling_info VARCHAR(150);
+  	last_name VARCHAR(100),
+  	address VARCHAR(150),
+  	biling_info VARCHAR(150);
   
 -- order table layout --
 
 CREATE TABLE order_(
-  order_id SERIAL PRIMARY KEY
-  order_date DATE(current_date)
-  sub_total NUMERIC(8,2)
-  total_cost NUMERIC(10,2)
-  car_serial_id INTEGER NOT NULL,
-  FOREIGN KEY(car_serial_id) REFERENCES model(car_serial_id);
+  	order_id SERIAL PRIMARY KEY
+  	order_date DATE(current_date)
+  	sub_total NUMERIC(8,2)
+  	total_cost NUMERIC(10,2)
+  	car_serial_id INTEGER NOT NULL,
+  	FOREIGN KEY(car_serial_id) REFERENCES model(car_serial_id);
   
   -- salesperson table layout --
   
-  CREATE TABLE salesperson(
-    salesperson_id SERIAL PRIMARY KEY
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
-    contact_info VARCHAR(250),
-    commission NUMERIC(10,2);
+ CREATE TABLE salesperson(
+    	salesperson_id SERIAL PRIMARY KEY
+    	first_name VARCHAR(100),
+    	last_name VARCHAR(100),
+    	contact_info VARCHAR(250),
+    	commission_rate NUMERIC(10,2);
     
  -- maintenance table layout --
  
  CREATE TABLE maintenance(
-  service_ticket_id SERIAL PRIMARY KEY
-  repairs_cost NUMERIC(8,2)
-  mechanic_name VARCHAR(100)
-  car_serial_id INTEGER NOT NULL,
-  FOREIGN KEY(car_serial_id) REFERENCES model(car_serial_id);
+  	service_ticket_id SERIAL PRIMARY KEY
+  	repairs_cost NUMERIC(8,2)
+ 	 mechanic_name VARCHAR(100)
+  	car_serial_id INTEGER NOT NULL,
+  	FOREIGN KEY(car_serial_id) REFERENCES model(car_serial_id);
   
 -- product table layout --
 
@@ -53,5 +53,5 @@ CREATE TABLE product(
 -- inventory table layout --
 
 CREATE TABLE inventory(
-  car_part_id SERIAL PRIMARY KEY
-  amount NUMERIC(8,2)
+  	car_part_id SERIAL PRIMARY KEY
+  	amount NUMERIC(8,2)
